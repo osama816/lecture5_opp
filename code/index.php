c98ced8ff5a5f7349ebd6b7177248aea634b4a71
osama elgendy <elgendyo240.com>
2025-05-24 04:29:22 +0300

    update

--- code/index.php
+++ code/index.php
@@ -1,24 +1,3 @@
-<?php
-
-require_once "vendor/autoload.php";
-require_once "config/database.php";
-
-use App\Database;
-use App\Product;
-
-$db = Database::get_instance($config)->get_connection();
-
-// $new_product= Product::create($db,"test",100,200,20);
-//$product=Product::find_by_id($db,1);
-//var_dump($product);
-
-foreach (Product::get_All($db) as $product) :
-    // echo $value->get_name()."      ".$value->get_price();
-
-    // var_dump(Product::get_All($db));
-
-
-?>
     <!DOCTYPE html>
     <html lang="en">
 
@@ -32,27 +11,63 @@ foreach (Product::get_All($db) as $product) :
     </head>
 
     <body>
-    <div class="col mb-5">
-        <div class="card h-100">
-            <!-- Product details-->
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <!-- Product name-->
-                    <h5 class="fw-bolder"><?= $product->get_name ?></h5>
-                    <!-- Product reviews-->
-                    <div class="d-flex justify-content-center small text-warning mb-2">
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                    </div>
-                    <!-- Product price-->
-                    <span
-                        class="text-muted text-decoration-line-through">$<?= $product->price_before_discount ?></span>
-                    <?= $product->get_price ?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 ">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">All Product </h1>
+
                 </div>
             </div>
+        </header>
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"></div>
+                <?php
 
+                require_once "vendor/autoload.php";
+                require_once "config/database.php";
+
+                use App\Database;
+                use App\Product;
+
+                $db = Database::get_instance($config)->get_connection();
+
+                // $new_product= Product::create($db,"phone",100,200,20);
+                //$product=Product::find_by_id($db,1);
+                //var_dump($product);
+
+                foreach (Product::get_All($db) as $product) :
+                    // echo $value->get_name()."      ".$value->get_price();
+
+                    // var_dump(Product::get_All($db));
+
+
+                ?>
+
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $product->get_name() ?></h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    <span
+                                        class="text-muted text-decoration-line-through">$<?= $product->price_before_discount() ?></span>
+                                    <?= $product->get_price() ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </section>
     </body>
-        <?php endforeach; ?>
+    <?php endforeach; ?>
